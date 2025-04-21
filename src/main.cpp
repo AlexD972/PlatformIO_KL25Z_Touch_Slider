b6f88e94d46cbce9876f7852abf4441418d5a1e5
DUCLOS Alexandre <alexandreduclos83@gmail.com>
2025-04-21 19:42:19 +0200

    Led RGB qui s'allume suivant la position sur le TouchSlider avec un printf qui fonctionne

--- src/main.cpp
+++ src/main.cpp
@@ -13,18 +13,23 @@ int main() {
     LedG = 1; // Turn off Green LED
     LedB = 1; // Turn off Blue LED
 
-    float position;
+    float position = 1.0f;
+
     printf("Test depuis main\r\n");
 
     while (true) 
     {
         position=lire_slider();
+        //printf("Position : %d.%02d\r\n", (int)position, (int)(position * 100) % 100);
+        
+        ThisThread::sleep_for(1000ms);
+        
 
-        if(position < 50) {
+        if(position < 0.5) {
             LedR = 0; // Turn on Red LED
             LedG = 1; // Turn off Green LED
             LedB = 1; // Turn off Blue LED
-        } else if (position < 100) {
+        } else if (position <= 0.99) {
             LedR = 1; // Turn off Red LED
             LedG = 0; // Turn on Green LED
             LedB = 1; // Turn off Blue LED
